c6afc4831ec63c6e37f2752bc75605860f621af1
tan <cupitrh@gmail.com>
2022-09-30 10:16:24 +0800

    content preview in list

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
     theme: {
         extend: {},
     },
-    plugins: [require("daisyui")],
+    plugins: [require("@tailwindcss/line-clamp"), require("daisyui")],
     daisyui: {
         themes: [
             {
